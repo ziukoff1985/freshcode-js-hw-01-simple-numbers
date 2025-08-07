aa33b4a0a5886fb2832a395bbb6e38a615869900
Borys Ziukov <ziukoff1985@gmail.com>
2025-08-07 19:46:54 +0300

    Code review

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 'use strict';
 
+console.log('List of all prime numbers from 1 to 100:');
+
 outer: for (let i = 2; i <= 100; i++) {
     for (let j = 2; j < i; j++) {
         if (i % j === 0) {
             continue outer;
         }
     }
+
     console.log(i);
 }
